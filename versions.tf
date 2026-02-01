5c718fdd37e7c41d5acbab2f51389e262dd61d53
Jari Jokinen <jari@j2digital.com>
2026-02-01 15:41:17 +0200

    Upgrade versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.11"
+  required_version = ">= 1.14"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.89.0"
+      version = "~> 6.30.0"
     }
   }
 }
